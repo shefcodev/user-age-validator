1c294e2a99537af6f01590910fb7ecb5eb3c5dd0
shefcodev <sherefjimoh@gmail.com>
2023-09-11 22:56:13 +0100

    styled add-user component

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import Button from '../UI/'
+import Button from '../UI/Button/Button';
 
 const AddUser = () => {
   return (
     <form>
       <label htmlFor='username'>Username</label>
-      <input type='text' id='username' />
-      <label htmlFor='age'>Age</label>
-      <input type='number' id='age' />
+      <input type='text' id='username' className='input' />
+      <label htmlFor='age'>Age (Years)</label>
+      <input type='number' id='age' className='input' />
+      <Button>Add User</Button>
     </form>
   );
 };
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Button = ({ children }) => {
-  return <button>{children}</button>;
+  return <button className='button'>{children}</button>;
 };
 
 export default Button;
