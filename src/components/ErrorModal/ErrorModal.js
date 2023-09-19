d9c02b6a86da50844eb366c89c029edd993f4854
shefcodev <sherefjimoh@gmail.com>
2023-09-19 13:31:57 +0100

    implemented the error-modal component logic

--- src/components/ErrorModal/ErrorModal.js
+++ src/components/ErrorModal/ErrorModal.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Button from '../UI/Button/Button';
 
 const ErrorModal = () => {
+  let clicked = false;
+
   const submitHandler = (event) => {
     event.preventDefault();
+    clicked = true;
   };
 
   return (
-    <div className='overlay'>
+    <div className='overlay' style={{ display: `${clicked && 'none'}` }}>
       <form className={'error'} onSubmit={submitHandler}>
         <div className={'invalid'}>
           <h3>Invalid Input</h3>
